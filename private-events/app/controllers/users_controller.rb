2e166445f65b4fcf616d8bbfc604bd61c6084242
Andrew Chung <andrew.chung159@gmail.com>
2015-04-06 14:44:08 -0400

    Create MVC for User and allow users to signup

---
+++ private-events/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = 'Account created!'
+			redirect_to @user
+		else
+			flash.now[:danger] = 'Could not create account!'
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :email)
+		end
+end
