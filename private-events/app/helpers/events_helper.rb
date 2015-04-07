ec2f5db13626f482acf25e0167fac618a37618e4
Andrew Chung <andrew.chung159@gmail.com>
2015-04-07 12:16:09 -0400

    Separate events into past and upcoming

--- private-events/app/helpers/events_helper.rb
+++ private-events/app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+	def prev_events
+		current_user.attended_events.where("date < ? ", DateTime.now)
+	end
+
+	def upcoming_events
+		current_user.attended_events.where("date >= ? ", DateTime.now)
+	end
 end
--- private-events/app/models/event.rb
+++ private-events/app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
 	belongs_to :creator, :class_name => "User"
 	has_many :invitations, :foreign_key => :attended_event_id
 	has_many :attendees, :through => :invitations
+
+	scope :past, 		 -> { where("date < ?", DateTime.now) }
+	scope :upcoming, -> { where("date >= ?", DateTime.now) }
 end
